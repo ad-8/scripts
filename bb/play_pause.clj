2cc7a57aff4404bf2506d7ae1e5c50f9d2f5a3af
arthur <arthurfleck8@proton.me>
2024-07-22 23:20:05 +0200

    refactor

--- bb/play_pause.clj
+++ bb/play_pause.clj
@@ -4,20 +4,23 @@
   (:require [clojure.string :as str]
             [babashka.process :refer [shell]]))
 
-; replace-id is just some random number (that does not interfere with other replacement ids)
-(def ntfy-play "notify-send playerctl Playing... --app-name=play-pause --expire-time=2000 --icon exaile-play --replace-id=123 --urgency=low")
-(def ntfy-pause "notify-send playerctl Pausing... --app-name=play-pause --expire-time=2000 --icon exaile-pause --replace-id=123 --urgency=low")
-(def status (-> (shell {:out :string} "playerctl status")
-                :out
-                str/trim))
+(def state->actions
+  {"play"  ["notify-send playerctl Playing... --app-name=play-pause --expire-time=2000 --icon exaile-play --replace-id=123 --urgency=low" 
+            "playerctl play"]
+   "pause" ["notify-send playerctl Pausing... --app-name=play-pause --expire-time=2000 --icon exaile-pause --replace-id=123 --urgency=low" 
+            "playerctl pause"]})
+
+(defn execute-commands [cmds]
+  (doseq [cmd cmds]
+    (shell cmd)))
 
 (defn set-player [new-state]
-  (case new-state
-    "play"  (and (shell ntfy-play) (shell "playerctl play"))
-    "pause" (and (shell ntfy-pause) (shell "playerctl pause"))
+  (if-let [commands (get state->actions new-state)]
+    (execute-commands commands)
     :error))
 
-(case status
-  "Playing" (set-player "pause")
-  "Paused"  (set-player "play")
-  :unknown-status)
+(let [status (-> (shell {:out :string} "playerctl status") :out str/trim)]
+  (case status
+    "Playing" (set-player "pause")
+    "Paused"  (set-player "play")
+    :unknown-status))
