8441190223b0cc70d0b2ad5c19b2260fbc5ae0d3
arthur <arthurfleck8@proton.me>
2024-09-17 21:13:09 +0200

    update statusbar

--- bb/dwl_status.clj
+++ bb/dwl_status.clj
@@ -7,12 +7,20 @@
         'java.time.format.DateTimeFormatter)
 
 
-(def formatter (DateTimeFormatter/ofPattern "dd.MM. HH:mm:ss"))
+;(def formatter (DateTimeFormatter/ofPattern "dd.MM. HH:mm:ss"))
+(def formatter (DateTimeFormatter/ofPattern " dd.MM.  HH:mm"))
 (def fmt-sec (DateTimeFormatter/ofPattern "ss"))
 
+;; echo " $(cat /tmp/licht-curr-val)"
 
-#_(shell "/home/ax/scripts/bb/i3vpn.clj")
+; (shell {:out :string} "cat /tmp/licht-curr-val")
 
+
+(defn shell-out [cmd freq curr-sec]
+  (if (= 0 (mod curr-sec freq))
+    (->> (shell {:out :string} cmd) 
+         :out str/split-lines first)
+    "..."))
 
 
 ; dwlb -status all 'text ^bg(ff0000)^lm(foot)text^bg()^lm() text'
@@ -20,14 +28,20 @@
   (let [now (LocalDateTime/now)
         date (.format now formatter)
         sec (Integer/parseInt (.format now fmt-sec))
-        vpn (when (= 0 (mod sec 10)) (->> (shell {:out :string} "/home/ax/scripts/bb/i3vpn.clj dwm") :out str/split-lines first))
-        space (when (= 0 (mod sec 10)) (->> (shell {:out :string} "/home/ax/scripts/bb/dwm_disk_space.clj") :out str/split-lines first))
-        fmt (format "dwlb -status all '^fg(ff0000)^lm(foot) foo-err^bg()^fg()^lm() | bla | %s | %s | %s '" 
-                    vpn space date)
-        
+
+        vpn (shell-out "/home/ax/scripts/bb/i3vpn.clj dwm" 30 sec)
+        weather (shell-out "/home/ax/scripts/bb/weather.clj dwm" 30 sec)
+        space (shell-out "/home/ax/scripts/bb/dwm_disk_space.clj" 30 sec)
+        licht (shell-out "cat /tmp/licht-curr-val" 30 sec)
+        volume (shell-out "/home/ax/scripts/dwm-volume.sh" 1 sec)
+
+        fmt (format
+             "dwlb -status all '^fg(a3be8c)%s^fg(4c566a) | ^lm(pavucontrol)%s ^lm()|  %s^bg()^lm() | %s | %s | ^fg(81a1c1) %s '"
+             weather volume licht vpn space date)
+
         ;
         ]
-    (if (= 0 (mod sec 10))
+    (if (= 0 (mod sec 30))
       (shell fmt)
       (Thread/sleep 1000))))
 
