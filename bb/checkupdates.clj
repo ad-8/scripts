fcd7a940fa3cb176e62730a8b49fbc4b07e4f282
arthur <arthurfleck8@proton.me>
2025-05-14 08:10:51 +0200

    improve output and refactor

--- bb/checkupdates.clj
+++ bb/checkupdates.clj
@@ -3,25 +3,29 @@
 (ns checkupdates
   (:require [clojure.string :as str]
             [clojure.pprint]
-            [babashka.process :refer [shell sh]]))
+            [babashka.process :refer [sh]]))
 
 
 (defn parse-line [line]
-  (let [parts (-> line
-                  (str/replace #" ->" "")
-                  (str/split #"\s"))]
+  (let [[pkg old new] (-> line
+                          (str/replace #" ->" "")
+                          (str/split #"\s"))]
+    {:pkg pkg
+     :old old
+     :new new}))
 
-    {:pkg (first parts)
-     :old (second parts)
-     :new (last parts)}))
+
+(defn print-updates [lines]
+  (if (= [""] lines)
+    (println "no updates")
+    (do (clojure.pprint/print-table (map parse-line lines))
+        (printf "\n\nupdates: %d\n" (count lines)))))
 
 
-(let [lines (-> (shell {:out :string} "checkupdates")
-                :out
-                str/split-lines)
+;; sh works better than shell when working with empty :out or :err
+(let [lines (-> (sh ["checkupdates"]) :out str/split-lines)
       news (-> (sh ["paru" "--show" "--news"]) :err)]
 
-  (clojure.pprint/print-table (map parse-line lines))
-  (printf "\n\nupdates: %d\n" (count lines))
+  (print-updates lines)
   (println "\n---\n")
   (println (str/trim news)))
