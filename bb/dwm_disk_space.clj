2f07cf3c3ed09b55fca68ddd18b096f5b63fa2d4
arthur <arthurfleck8@proton.me>
2025-07-11 12:21:33 +0200

    add json output

--- bb/dwm_disk_space.clj
+++ bb/dwm_disk_space.clj
@@ -1,12 +1,28 @@
 #!/usr/bin/env bb
 (ns dwm-disk-space
   (:require [babashka.process :refer [shell]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [cheshire.core :as json]))
 
-(let [free-space-on-root (->> (shell {:out :string} "df -h / --output=avail")
-                              :out
-                              str/split-lines
-                              last
-                              str/trim)
-      fmt (format "󰋊 %s" free-space-on-root)]
-  (println fmt))
+(defn print-free-space [the-format]
+  (let [free-space-on-root (->> (shell {:out :string} "df -h / --output=avail")
+                                :out
+                                str/split-lines
+                                last
+                                str/trim)
+        fmt (format "󰋊 %s" free-space-on-root)
+        free-space-int (Integer/parseInt (re-find #"\d+" free-space-on-root))
+        class (if (< free-space-int 25) :low :ok)
+        json (json/encode {:text fmt :class class})]
+
+    (if (= "json" the-format)
+      (println json)
+      (println fmt))))
+
+(defn run []
+  (let [args *command-line-args*]
+    (if (= "json" (first args))
+      (print-free-space "json")
+      (print-free-space "text"))))
+
+(run)
