d88648d7abd71cc0450d582701a9c3a4082be3bb
arthur <arthurfleck8@proton.me>
2025-07-30 01:33:02 +0200

    add ip script

---
+++ bb/ip_addr.clj
@@ -0,0 +1,26 @@
+#!/usr/bin/env bb
+(ns ip-addr
+  (:require [babashka.process :refer [shell]]
+            [clojure.string :as str]))
+
+
+;; ruby version
+;; res = `ip -o -4 addr show | awk '{print $2, $4}' | cut -d/ -f1`
+
+;; ips = res.lines
+;;     .reject { |line| line.match /^(lo|docker)/ }
+;;     .map { |line| line.strip }
+
+;; foo = ips.join(", ")
+;; puts foo
+
+
+(->> (shell {:out :string} "ip -o -4 addr show")
+     :out
+     str/split-lines
+     (map #(str/split % #"\s+"))
+     (map (fn [[_ ifn _ ip & _rest]] [ifn (-> ip (str/split #"/") first)]))
+     (remove (fn [[ifn _ip]] (re-find #"lo|docker" ifn)))
+     (map (fn [args] (str/join " " args)))
+     (str/join ", ")
+     println)
