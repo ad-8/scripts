61ae65aba2938612d21cc6de4b07196680f8accb
arthur <arthurfleck8@proton.me>
2025-09-12 09:17:43 +0200

    add interface to exclude

--- bb/ip_addr.clj
+++ bb/ip_addr.clj
@@ -21,7 +21,7 @@
      (map #(str/split % #"\s+"))
      (map (fn [[_ ifn _ ip & _rest]]
             [ifn (-> ip (str/split #"/") first)]))
-     (remove (fn [[ifn _ip]] (re-find #"lo|docker|virbr" ifn)))
+     (remove (fn [[ifn _ip]] (re-find #"lo|docker|virbr|br" ifn)))
      #_(filter (fn [[ifn _ip]]
                (re-find #"enp1s0|wlan0|wlp3s0|muc" ifn)))
      (map (fn [[ifn ip]]
