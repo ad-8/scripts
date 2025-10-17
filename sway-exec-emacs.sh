cae995f65985138b5d986ef56631268281c5c650
arthur <arthurfleck8@proton.me>
2025-10-17 18:36:26 +0200

    retire more

--- sway-exec-emacs.sh
+++ sway-exec-emacs.sh
@@ -3,8 +3,7 @@
 emacs --daemon &
 
 sleep 5
-
-emacsclient -c -F '((title . "emacs-scratchpad-todo"))' ~/sync/TODO.org
+emacsclient -c -F '((title . "emacs-scratchpad-todo"))' ~/sync/TODO.org 2>&1 >> /tmp/emacs-scratchpad-script.log
 sleep 5
-emacsclient -c -F '((title . "scratchmacs"))'
+emacsclient -c -F '((title . "scratchmacs"))' 2>&1 >> /tmp/emacs-scratchpad-script.log
 sleep 3
