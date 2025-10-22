42132cd8d78d2b99dbdb016da8a0a0b41f036cc5
arthur <arthurfleck8@proton.me>
2025-10-22 17:13:44 +0200

    clean up

--- waybar.clj
+++ waybar.clj
@@ -2,17 +2,10 @@
 (ns waybar
   (:require [clojure.string :as str]
             [babashka.process :refer [shell]]
-            [clojure.java.io :as io]
-            [babashka.fs :as fs]
-            [babashka.cli :as cli]
-            [clojure.test :refer :all]
-            [babashka.http-client :as http]
-            [cheshire.core :as json]
-            [clojure.edn :as edn]
-            [clojure.walk :refer [keywordize-keys]]))
+            [cheshire.core :as json]))
 
 (defn default []
-  (println "default case"))
+  (println "error-unknown-action"))
 
 (defn waybar-date []
   (let [date-str (-> (shell {:out :string} "date '+%a %d.%m.'")
@@ -126,11 +119,6 @@
             (let [status (-> (shell {:out :string} "sh -c 'setsid waybar >/dev/null 2>&1 &'") :exit)]
               (printf "killing waybar, status = %d\n" status)))))))
 
-(comment
-  (-> (shell {:out :string :continue true} "sh -c 'pgrep waybar >/dev/null'")
-                   :exit)
-  ;; 
-  )
 
 ; match e.g.: ["ProtonVPN DE#316" "DE#316"]
 (defn waybar-vpn []
