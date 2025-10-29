9ad81b0a896ab8e1f1d2559fff57e49420f72d62
arthur <arthurfleck8@proton.me>
2025-10-29 10:57:02 +0100

    rm script

--- sway-exec-emacs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-emacs --daemon &
-
-sleep 5
-emacsclient -c -F '((title . "emacs-scratchpad-todo"))' ~/sync/TODO.org 2>&1 >> /tmp/emacs-scratchpad-script.log
-sleep 5
-emacsclient -c -F '((title . "scratchmacs"))' 2>&1 >> /tmp/emacs-scratchpad-script.log
-sleep 3
