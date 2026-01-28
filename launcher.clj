d198a591ee83bfc936612f307373ec864139f22e
arthur <arthurfleck8@proton.me>
2026-01-28 23:41:15 +0100

    update launcher

--- launcher.clj
+++ launcher.clj
@@ -7,16 +7,19 @@
 ;; which are still missing in niri
 
 (def cmds
-  {
-   "bluetui" "alacritty -T ax-bluetui -e bluetui"
+  {"bluetui" "alacritty -T ax-bluetui -e bluetui"
    "brave" "brave"
    "emacsclient" "emacsclient -c"
    "firefox" "firefox"
    "thunar (file manager)" "thunar"
-   })
+   "volume control (pavu)" "pavucontrol"
+   "rofi - files" "rofi -show recursivebrowser"
+   "rofi - windows" "rofi -show window"
+   "linkding std" "bb /home/ax/x/ax_bookmarks.clj std"
+   "linkding archived" "bb /home/ax/x/ax_bookmarks.clj archived"})
 
-(let [user-choice (-> (process "echo -e" (str/join "\n" (keys cmds)))
-                      (process {:out :string} "wmenu -i -l 15")
+(let [user-choice (-> (process "echo -e" (str/join "\n" (sort (keys cmds))))
+                      (process {:out :string} "rofi -dmenu -p mode-open")
                       deref :out str/trim)
       cmd-to-run (get cmds user-choice)]
   (println user-choice "---" cmd-to-run)
